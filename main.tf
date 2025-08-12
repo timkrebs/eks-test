e0a40146ed2a76b15657891a4c8ddb60f128fae8
Tim Krebs <tim.krebs@hashicorp.com>
2025-08-12 10:45:36 +0200

    Remove HCP Vault and associated networking resources from Terraform configuration; add comment for EBS CSI Driver setup.

--- main.tf
+++ main.tf
@@ -69,6 +69,7 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
+
   }
 
   eks_managed_node_groups = {
@@ -94,7 +95,8 @@ module "eks" {
   }
 }
 
-# EBS CSI Driver setup
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
@@ -108,94 +110,4 @@ module "irsa-ebs-csi" {
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-# HCP Vault Dedicated Configuration
-resource "hcp_vault_cluster" "main" {
-  cluster_id      = "vault-cluster-${random_string.suffix.result}"
-  hvn_id          = hcp_hvn.main.hvn_id
-  tier            = var.vault_tier
-  public_endpoint = true
-}
-
-# HCP HVN (HashiCorp Virtual Network)
-resource "hcp_hvn" "main" {
-  hvn_id         = "main-hvn-${random_string.suffix.result}"
-  cloud_provider = "aws"
-  region         = var.region
-  cidr_block     = "172.25.16.0/20"
-}
-
-# HVN Route to VPC
-resource "hcp_hvn_route" "main" {
-  hvn_link         = hcp_hvn.main.self_link
-  hvn_route_id     = "main-hvn-route-${random_string.suffix.result}"
-  destination_cidr = "10.0.0.0/16"
-  target_link      = hcp_aws_network_peering.peer.self_link
-}
-
-# AWS Network Peering
-resource "hcp_aws_network_peering" "peer" {
-  hvn_id          = hcp_hvn.main.hvn_id
-  peering_id      = "peer-${random_string.suffix.result}"
-  peer_vpc_id     = module.vpc.vpc_id
-  peer_account_id = data.aws_caller_identity.current.account_id
-  peer_vpc_region = var.region
-}
-
-# Accept the VPC peering connection
-resource "aws_vpc_peering_connection_accepter" "peer" {
-  vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
-  auto_accept               = true
-
-  tags = {
-    Name = "HCP peering (${hcp_aws_network_peering.peer.peering_id})"
-  }
-}
-
-# Route table entries for peering connection
-resource "aws_route" "private_subnets_to_hvn" {
-  count                     = length(module.vpc.private_route_table_ids)
-  route_table_id            = module.vpc.private_route_table_ids[count.index]
-  destination_cidr_block    = hcp_hvn.main.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer.id
-}
-
-resource "aws_route" "public_subnets_to_hvn" {
-  count                     = length(module.vpc.public_route_table_ids)
-  route_table_id            = module.vpc.public_route_table_ids[count.index]
-  destination_cidr_block    = hcp_hvn.main.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer.id
-}
-
-# Data source for AWS caller identity
-data "aws_caller_identity" "current" {}
-
-# Vault Admin Token for initial setup
-resource "hcp_vault_cluster_admin_token" "main" {
-  cluster_id = hcp_vault_cluster.main.cluster_id
-}
-
-# IAM role for Vault Secrets Operator
-resource "aws_iam_role" "vault_secrets_operator" {
-  name = "vault-secrets-operator-${random_string.suffix.result}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Effect = "Allow"
-        Principal = {
-          Federated = module.eks.oidc_provider_arn
-        }
-        Condition = {
-          StringEquals = {
-            "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub" = "system:serviceaccount:vault-secrets-operator-system:vault-secrets-operator-controller-manager"
-            "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:aud" = "sts.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
 }
